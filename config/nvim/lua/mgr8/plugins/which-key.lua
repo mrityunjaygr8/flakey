ccc15ad9030472ae9f7c0da320ebfb616c6dff1e
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-04-08 17:22:41 +0530

    using leader s for search; tab and split which key items removed

--- config/nvim/lua/mgr8/plugins/which-key.lua
+++ config/nvim/lua/mgr8/plugins/which-key.lua
@@ -10,14 +10,12 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		require("which-key").register({
-			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
 			["<leader>c"] = { name = "[C]ode Actions", _ = "which_key_ignore" },
 			["<leader>e"] = { name = "[E]xplorer", _ = "which_key_ignore" },
-			["<leader>m"] = { name = "Format File", _ = "which_key_ignore" },
+			["<leader>f"] = { name = "[F]ormat File", _ = "which_key_ignore" },
 			["<leader>n"] = { name = "Clear Search Highlights", _ = "which_key_ignore" },
 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]plits", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]abs", _ = "which_key_ignore" },
 			["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
 			["<leader>x"] = { name = "Trouble", _ = "which_key_ignore" },
 		})
