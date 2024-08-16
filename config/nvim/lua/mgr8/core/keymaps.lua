0efdd5c7cc33265b3e25dd17ab3ae30ec2119c2f
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-08-16 13:36:47 +0530

    indent and outdent keep selection in nvim

--- config/nvim/lua/mgr8/core/keymaps.lua
+++ config/nvim/lua/mgr8/core/keymaps.lua
@@ -16,7 +16,10 @@ keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "[W]rite file" })
 keymap.set("n", "<leader>qb", "<cmd>bd<CR>", { desc = "[Q]uit [b]uffer" })
 keymap.set("n", "<leader>qa", "<cmd>bd<CR>", { desc = "[Q]uit [a]ll buffers" })
 -- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x')
+
+keymap.set("v", ">", ">gv", { desc = "indent " })
+keymap.set("v", "<", "<gv", { desc = "outdent " })
 
 -- -- window management
 -- keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
