afbb46c355a66ef6ea9c8d905d85c1e311e5aaac
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-09-16 15:53:42 +0530

    gnome-tweaks is being used as top level

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -140,7 +140,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     openssl
-    gnome.gnome-tweaks
+    gnome-tweaks
     gnomeExtensions.appindicator
     gnomeExtensions.pano
     gnomeExtensions.blur-my-shell
