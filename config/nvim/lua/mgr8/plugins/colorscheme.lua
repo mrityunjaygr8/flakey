fba2a3601f4c21d234ce564c7b72e7da977a9a53
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-01-16 13:21:15 +0530

    nvim flexoki replaces catpuccin

--- config/nvim/lua/mgr8/plugins/colorscheme.lua
+++ config/nvim/lua/mgr8/plugins/colorscheme.lua
@@ -1,11 +1,27 @@
+-- return {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("catppuccin").setup({
+-- 			default_integrations = true,
+-- 		})
+-- 		vim.cmd([[colorscheme catppuccin-mocha]])
+-- 	end,
+-- }
+
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"nuvic/flexoki-nvim",
+	name = "flexoki",
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			default_integrations = true,
+		require("flexoki").setup({
+			variant = "moon",
+			enable = {
+				terminal = true,
+			},
 		})
-		vim.cmd([[colorscheme catppuccin-mocha]])
+
+		vim.cmd([[colorscheme flexoki]])
 	end,
 }
