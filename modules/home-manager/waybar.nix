41784a7236bd6d863c6b0af0b83dcc0102489ff9
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-01-18 15:00:51 +0530

    waybar modules added

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
+  home.packages = with pkgs; [overskride pavucontrol networkmanager];
   programs.waybar = {
     enable = true;
     settings = {
@@ -6,13 +7,133 @@
         layer = "top";
         position = "top";
         height = 30;
-        modules-left = ["hyprland/workspaces" "sway/mode" "wlr/taskbar"];
-        modules-center = ["sway/window" "custom/hello-from-waybar"];
-        modules-right = ["mpd" "custom/mymodule#with-css-id" "temperature"];
+        modules-left = ["hyprland/workspaces" "hyprland/submap" "wlr/taskbar"];
+        modules-center = ["hyprland/window" "clock"];
+        modules-right = ["memory" "cpu" "mpd" "network" "pulseaudio" "bluetooth" "battery" "privacy" "idle_inhibitor" "temperature"];
 
-        "sway/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
+        # "battery" = {
+        #
+        # };
+
+        "pulseaudio" = {
+          "format" = "{volume}% {icon}";
+          "format-bluetooth" = "{volume}% {icon}";
+          "format-muted" = "";
+          "format-icons" = {
+            "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
+            "alsa_output.pci-0000_00_1f.3.analog-stereo-muted" = "";
+            "headphone" = "";
+            "hands-free" = "";
+            "headset" = "";
+            "phone" = "";
+            "phone-muted" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = ["" ""];
+          };
+          "scroll-step" = 1;
+          "on-click" = "pavucontrol";
+          "ignored-sinks" = ["Easy Effects Sink"];
+        };
+
+        "network" = {
+          "format" = "{ifname}";
+          "format-wifi" = "{essid} ({signalStrength}%) ";
+          "format-ethernet" = "{ipaddr}/{cidr} 󰊗";
+          "format-disconnected" = "Disconnected"; #An empty format will hide the module.
+          "tooltip-format" = "{ifname} via {gwaddr} 󰊗";
+          "tooltip-format-wifi" = "{essid} ({signalStrength}%) ";
+          "tooltip-format-ethernet" = "{ifname} ";
+          "tooltip-format-disconnected" = "Disconnected";
+          "max-length" = 50;
+        };
+
+        "privacy" = {
+          "icon-spacing" = 4;
+          "icon-size" = 18;
+          "transition-duration" = 250;
+          "modules" = [
+            {
+              "type" = "screenshare";
+              "tooltip" = true;
+              "tooltip-icon-size" = 24;
+            }
+            {
+              "type" = "audio-out";
+              "tooltip" = true;
+              "tooltip-icon-size" = 24;
+            }
+            {
+              "type" = "audio-in";
+              "tooltip" = true;
+              "tooltip-icon-size" = 24;
+            }
+          ];
+        };
+
+        "cpu" = {
+          "interval" = 10;
+          "format" = "{}% ";
+          "max-length" = 10;
+        };
+
+        "memory" = {
+          "interval" = 30;
+          "format" = "{used:0.1f}G/{total:0.1f}G ";
+        };
+
+        "idle_inhibitor" = {
+          "format" = "{icon}";
+          "format-icons" = {
+            "activated" = "";
+            "deactivated" = "";
+          };
+        };
+
+        "clock" = {
+          "format" = "{:%H:%M}  ";
+          "format-alt" = "{:%A, %B %d, %Y (%R)}  ";
+          "tooltip-format" = "<tt><small>{calendar}</small></tt>";
+          "calendar" = {
+            "mode" = "year";
+            "mode-mon-col" = 3;
+            "weeks-pos" = "right";
+            "on-scroll" = 1;
+            "format" = {
+              "months" = "<span color='#ffead3'><b>{}</b></span>";
+              "days" = "<span color='#ecc6d9'><b>{}</b></span>";
+              "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
+              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+              "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          "actions" = {
+            "on-click-right" = "mode";
+            "on-scroll-up" = ["tz_up" "shift_up"];
+            "on-scroll-down" = ["tz_down" "shift_down"];
+          };
+        };
+
+        "bluetooth" = {
+          "format" = " {status}";
+          "format-connected" = " {device_alias}";
+          "format-connected-battery" = " {device_alias} {device_battery_percentage}%";
+          # "format-device-preference"= [ "device1"; "device2" ], // preference list deciding the displayed device
+          "tooltip-format" = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
+          "tooltip-format-connected" = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+          "tooltip-format-enumerate-connected" = "{device_alias}\t{device_address}";
+          "tooltip-format-enumerate-connected-battery" = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
+          "on-click" = "overskride";
+        };
+
+        "hyprland/window" = {
+          "separate-outputs" = true;
+        };
+
+        "hyprland/workspaces" = {
+          format = "{icon}";
+          on-scroll-up = "hyprctl dispatch workspace e+1";
+          on-scroll-down = "hyprctl dispatch workspace e-1";
         };
         "custom/hello-from-waybar" = {
           format = "hello {}";
