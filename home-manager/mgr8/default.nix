c275c835237b170abaaf5cbd2ec906a79a7137f1
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-01-19 19:05:37 +0530

    cosmic home manager module removed

--- home-manager/mgr8/default.nix
+++ home-manager/mgr8/default.nix
@@ -19,7 +19,7 @@
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
     ../common
-    ../../modules/home-manager/cosmic.nix
+    # ../../modules/home-manager/cosmic.nix
   ];
 
   nixpkgs = {
