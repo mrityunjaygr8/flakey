1e40d0305d9f063a1a6239ad5f374d9d35e158c0
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-01-22 13:25:32 +0530

    mini.icons added, new which key spec adopted

--- config/nvim/lua/mgr8/plugins/which-key.lua
+++ config/nvim/lua/mgr8/plugins/which-key.lua
@@ -1,28 +1,28 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  dependencies = {
+    'echasnovski/mini.icons',
+  },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
   config = function()
-    require("which-key").register({
-      J = {"mzJ`z", "Join lines and keep cursor position"},
-      ["<C-d>"] = {"<C-d>zz", "Half page down and center"},
-      ["<C-u>"] = {"<C-u>zz", "Half page up and center"},
-      n = { "nzzzv", "Next search result and center" },
-      N = { "Nzzzv", "Previous search result and center" },
-      ["<leader>"] = {
-        s = { name = "[S]earch", _ = "which_key_ignore" },
-        c = { name = "[C]ode Actions", _ = "which_key_ignore" },
-        e = { name = "[E]xplorer", _ = "which_key_ignore" },
-        f = { name = "[F]ormat File", _ = "which_key_ignore" },
-        n = { name = "Clear Search Highlights", _ = "which_key_ignore" },
-        r = { name = "[R]ename", _ = "which_key_ignore" },
-        h = { name = "[H]arpoon", _ = "which_key_ignore" },
-        x = { name = "Trouble", _ = "which_key_ignore" },
-      }
-      
+    require("which-key").add({
+      { "<C-d>", "<C-d>zz", desc = "Half page down and center" },
+      { "<C-u>", "<C-u>zz", desc = "Half page up and center" },
+      { "<leader>c", group = "[C]ode Actions" },
+      { "<leader>e", group = "[E]xplorer" },
+      { "<leader>f", group = "[F]ormat File" },
+      { "<leader>h", group = "[H]arpoon" },
+      { "<leader>n", group = "Clear Search Highlights" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>x", group = "Trouble" },
+      { "J", "mzJ`z", desc = "Join lines and keep cursor position" },
+      { "N", "Nzzzv", desc = "Previous search result and center" },
+      { "n", "nzzzv", desc = "Next search result and center" },
     })
   end,
 
