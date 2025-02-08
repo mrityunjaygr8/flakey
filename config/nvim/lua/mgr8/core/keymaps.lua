fd63334dee73035367a0f7929e3757fa57711f4f
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-02-08 13:46:02 +0530

    quit all buffers keymap command changed

--- config/nvim/lua/mgr8/core/keymaps.lua
+++ config/nvim/lua/mgr8/core/keymaps.lua
@@ -6,13 +6,12 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps --
 
-
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "[W]rite file" })
 keymap.set("n", "<leader>qb", "<cmd>bd<CR>", { desc = "[Q]uit [b]uffer" })
-keymap.set("n", "<leader>qa", "<cmd>bd<CR>", { desc = "[Q]uit [a]ll buffers" })
+keymap.set("n", "<leader>qa", "<cmd>bufdo bd<CR>", { desc = "[Q]uit [a]ll buffers" })
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
 
