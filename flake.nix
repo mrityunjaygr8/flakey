e63eea882e5817c690871f63d89239d2acce1c10
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-02-17 11:56:40 +0530

    cachix stuff added to top level flake config

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "Your new nix config";
 
+  nixConfig = {
+    extra-substitutors = [
+      "https://cache.nixos.org"
+      "https://cosmic.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     # Nixpkgs
     # You can access packages and modules from different nixpkgs revs
@@ -83,12 +96,6 @@
         specialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main nixos configuration file <
-          {
-            nix.settings = {
-              substituters = ["https://cosmic.cachix.org"];
-              trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
-            };
-          }
           nixos-cosmic.nixosModules.default
           ./nixos/kharkanas
           disko.nixosModules.disko
@@ -98,12 +105,6 @@
         specialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main nixos configuration file <
-          {
-            nix.settings = {
-              substituters = ["https://cosmic.cachix.org"];
-              trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
-            };
-          }
           nixos-cosmic.nixosModules.default
           ./nixos/black-coral
           disko.nixosModules.disko
