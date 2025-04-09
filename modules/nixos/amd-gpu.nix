c37ea80dfa18e89cfe05b376ce33de6671293447
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-04-09 15:31:30 +0530

    override added for using gpu

--- modules/nixos/amd-gpu.nix
+++ modules/nixos/amd-gpu.nix
@@ -6,5 +6,9 @@
   services.ollama = {
     enable = true;
     acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1031";
+    };
+    rocmOverrideGfx = "10.3.1";
   };
 }
