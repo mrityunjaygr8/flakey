1b29b50bf38c7488ed2a67a63179e883e4be03e3
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-15 20:33:02 +0530

    languages added to treesitter

--- config/nvim/lua/mgr8/plugins/treesitter.lua
+++ config/nvim/lua/mgr8/plugins/treesitter.lua
@@ -1,3 +1,59 @@
-require("lazy").setup({
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
-})
+return  {
+  "nvim-treesitter/nvim-treesitter", 
+  branch = 'master', 
+  lazy = false, 
+  build = ":TSUpdate",
+  config = function() 
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      ensure_installed = {
+        "lua",
+        "vim",
+        "elixir",
+        "heex",
+        "python",
+        "go",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "dockerfile",
+        "gitignore",
+        "rust",
+        "sql",
+        "csv",
+        "fish",
+        "json",
+        "json5",
+        "jsonc",
+        "nix",
+        "toml",
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+        },
+      },
+    })
+  end
+
+}
