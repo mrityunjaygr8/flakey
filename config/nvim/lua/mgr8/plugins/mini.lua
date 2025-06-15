759468ea23fca4cb7b02dae7c535dfd99a4db4ac
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-16 01:52:01 +0530

    mini surround and around/inside plugins activated

--- config/nvim/lua/mgr8/plugins/mini.lua
+++ config/nvim/lua/mgr8/plugins/mini.lua
@@ -1,7 +1,10 @@
 return {
-  'echasnovski/mini.nvim', version = false,
-  config = function()
-    require("mini.icons").setup()
-    require("mini.statusline").setup()
-  end
+	"echasnovski/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.icons").setup()
+		require("mini.statusline").setup()
+		require("mini.ai").setup()
+		require("mini.surround").setup()
+	end,
 }
