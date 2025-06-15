987cfc0856465f82c4fc4fb8be881066de13904c
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-15 22:15:33 +0530

    groups configuration added

--- config/nvim/lua/mgr8/plugins/whichkey.lua
+++ config/nvim/lua/mgr8/plugins/whichkey.lua
@@ -4,19 +4,23 @@ return { -- Useful plugin to show you pending keybinds.
     keys = {
       { "<C-d>", "<C-d>zz", desc = "Half page down and center" },
       { "<C-u>", "<C-u>zz", desc = "Half page up and center" },
-      { "<leader>c", group = "[C]ode Actions" },
-      { "<leader>e", group = "[E]xplorer" },
-      { "<leader>f", group = "[F]ormat File" },
-      { "<leader>h", group = "[H]arpoon" },
-      { "<leader>n", group = "Clear Search Highlights" },
-      { "<leader>r", group = "[R]ename" },
-      { "<leader>s", group = "[S]earch" },
-      { "<leader>x", group = "Trouble" },
       { "J", "mzJ`z", desc = "Join lines and keep cursor position" },
       { "N", "Nzzzv", desc = "Previous search result and center" },
       { "n", "nzzzv", desc = "Next search result and center" },
     },
     opts = {
+      spec = {
+        { "<leader>c", group = "[C]ode Actions" },
+        { "<leader>e", group = "[E]xplorer" },
+        { "<leader>f", group = "[F]ormat File" },
+        { "<leader>h", group = "[H]arpoon" },
+        { "<leader>n", group = "Clear/[N]o Search Highlights" },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>x", group = "Trouble" },
+        { "<leader>q", group = "[Q]uit" },
+        { "<leader>l", group = "[L]azygit" },
+      },
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
       delay = 500,
