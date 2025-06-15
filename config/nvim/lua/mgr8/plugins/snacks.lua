60d43ac2fab24a6b8b3bb7c90f5aaa959b35ae68
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-16 00:31:09 +0530

    some finding related keybinds added

--- config/nvim/lua/mgr8/plugins/snacks.lua
+++ config/nvim/lua/mgr8/plugins/snacks.lua
@@ -6,7 +6,14 @@ return {
     {"<leader>lg", function() Snacks.lazygit() end, desc = "Open lazygit"},
     {"<leader>ee", function() Snacks.explorer() end, desc = "Toggle the explorer"},
     {"<leader>ef", function() Snacks.explorer() end, desc = "Current File"},
-
+    -- find
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
   },
   ---@type snacks.Config
   opts = {
