d6a6ad882e6eb68283eff51e9ab3872158108bd3
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-16 01:43:11 +0530

    formatting done

--- config/nvim/lua/mgr8/plugins/todo-comments.lua
+++ config/nvim/lua/mgr8/plugins/todo-comments.lua
@@ -1,6 +1,22 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false },
-  keys = {
-    { "<leader>tt", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-    { "<leader>tT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
-  },
+return {
+	"folke/todo-comments.nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = { signs = false },
+	keys = {
+		{
+			"<leader>tt",
+			function()
+				Snacks.picker.todo_comments()
+			end,
+			desc = "Todo",
+		},
+		{
+			"<leader>tT",
+			function()
+				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
+	},
 }
