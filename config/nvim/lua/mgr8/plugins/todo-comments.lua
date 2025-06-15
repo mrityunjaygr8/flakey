0346dfdf21647ce3806368b195b3f81fe5306ca9
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-16 01:22:14 +0530

    todo-comments now working

--- config/nvim/lua/mgr8/plugins/todo-comments.lua
+++ config/nvim/lua/mgr8/plugins/todo-comments.lua
@@ -1,5 +1,4 @@
-return {
-  "folke/todo-comments.nvim",
+return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false },
   keys = {
     { "<leader>tt", function() Snacks.picker.todo_comments() end, desc = "Todo" },
     { "<leader>tT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
