a4ca5bde5a5688f883f881f1882858957f6243b1
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-15 23:28:56 +0530

    lsp config added

--- config/nvim/lua/mgr8/plugins/lsp.lua
+++ config/nvim/lua/mgr8/plugins/lsp.lua
@@ -11,5 +11,49 @@ return {
       },
     },
   },
+  { -- optional blink completion source for require statements and module annotations
+    "saghen/blink.cmp",
+    version = '1.*',
+    opts = {
+      fuzzy = {implementation = "lua"},
+      sources = {
+        -- add lazydev to your completion providers
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
 
+    -- example using `opts` for defining servers
+    opts = {
+      servers = {
+        lua_ls = {},
+        pyright = {},
+        gopls = {},
+        ts_ls = {},
+        nixd = {},
+        bashls = {},
+        terraformls = {}
+      }
+    },
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end
+  }
 }
