a77fe54cc67d1a98d4dbc2c17df88487ec191099
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-15 19:26:24 +0530

    more options added

--- config/nvim/lua/mgr8/core/options.lua
+++ config/nvim/lua/mgr8/core/options.lua
@@ -37,12 +37,15 @@ opt.inccommand = 'split'
 
 -- Show which line your cursor is on
 opt.cursorline = true
-opt.colorcolumn = 80
+opt.colorcolumn = "80"
 
 -- Minimal number of screen lines to keep above and below the cursor.
 opt.scrolloff = 20
 
-
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
@@ -56,3 +59,13 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
