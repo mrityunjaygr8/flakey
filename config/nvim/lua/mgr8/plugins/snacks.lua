20e8fc186fd4d4028a494f7550828cacf00a026f
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-15 21:05:35 +0530

    keybinds added for lazygit and explorer

--- config/nvim/lua/mgr8/plugins/snacks.lua
+++ config/nvim/lua/mgr8/plugins/snacks.lua
@@ -2,6 +2,12 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  keys = {
+    {"<leader>lg", function() Snacks.lazygit() end, desc = "Open lazygit"},
+    {"<leader>ee", function() Snacks.explorer() end, desc = "Toggle the explorer"},
+    {"<leader>ef", function() Snacks.explorer() end, desc = "Current File"},
+
+  },
   ---@type snacks.Config
   opts = {
     -- your configuration comes here
@@ -9,9 +15,10 @@ return {
     -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = {enabled = true, replace_netrw = true},
     indent = { enabled = true },
     input = { enabled = true },
+    lazygit = {enabled = true },
     picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
