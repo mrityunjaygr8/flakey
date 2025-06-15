3a9c83969112d903869b76b9b30492a44ff3b4ea
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-16 00:52:38 +0530

    todo-comments group changed to `t`

--- config/nvim/lua/mgr8/plugins/todo-comments.lua
+++ config/nvim/lua/mgr8/plugins/todo-comments.lua
@@ -1,8 +1,7 @@
 return {
   "folke/todo-comments.nvim",
-  optional = true,
   keys = {
-    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-    { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    { "<leader>tt", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    { "<leader>tT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
   },
 }
