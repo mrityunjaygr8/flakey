b2026550bb149a8ce1e8c15f8809d246f09627d6
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-06-16 11:07:26 +0530

    lualine added

--- config/nvim/lua/mgr8/plugins/mini.lua
+++ config/nvim/lua/mgr8/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
 	version = false,
 	config = function()
 		require("mini.icons").setup()
-		require("mini.statusline").setup()
+		-- require("mini.statusline").setup()
 		require("mini.ai").setup()
 		require("mini.surround").setup()
 	end,
