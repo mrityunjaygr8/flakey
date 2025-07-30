6b861c4cb59875f3d9bd5b9a8fd797dc489c11a3
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-07-30 13:14:34 +0530

    kde module added

---
+++ modules/nixos/kde.nix
@@ -0,0 +1,19 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  services.xserver.enable = true; # optional
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
+}
