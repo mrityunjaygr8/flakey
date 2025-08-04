e66488367d71c53f0c23e319e8576ca6fa65fe3b
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-08-04 10:50:53 +0530

    proper timeouts added for hypridle

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -95,11 +95,11 @@ in {
 
       listener = [
         {
-          timeout = 60;
+          timeout = 900;
           on-timeout = "hyprlock";
         }
         {
-          timeout = 90;
+          timeout = 1200;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
