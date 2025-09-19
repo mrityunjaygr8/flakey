77bc323bab33cecd9a29ddd0e43e9c733b164bfb
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-09-19 14:53:07 +0530

    ruff organize imports added to conform

--- config/nvim/lua/mgr8/plugins/conform.lua
+++ config/nvim/lua/mgr8/plugins/conform.lua
@@ -25,7 +25,7 @@ return { -- Autoformat
 			elixir = { "mix" },
 			python = function(bufnr)
 				if require("conform").get_formatter_info("ruff_format", bufnr).available then
-					return { "ruff_fix", "ruff_format" }
+					return { "ruff_fix", "ruff_format", "ruff_organize_imports" }
 				else
 					return { "isort", "black" }
 				end
