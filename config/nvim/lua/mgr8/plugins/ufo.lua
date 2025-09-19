64479e5f9a480b620d8e7d54386e881e311d53ec
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-09-19 14:36:09 +0530

    folding support added

--- config/nvim/lua/mgr8/plugins/ufo.lua
+++ config/nvim/lua/mgr8/plugins/ufo.lua
@@ -1,6 +1,33 @@
 return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = { "kevinhwang91/promise-async" },
+	event = "VimEnter",
+	keys = {
+		{
+			"zR",
+			function()
+				require("ufo").openAllFolds()
+			end,
+			desc = "Open All Folds",
+		},
+		{
+			"zN",
+			function()
+				require("ufo").closeAllFolds()
+			end,
+			desc = "Close All Folds",
+		},
+		{
+			"zK",
+			function()
+				local winid = require("ufo").peekFoldedLinesUnderCursor()
+				if not winid then
+					vim.lsp.buf.hover()
+				end
+			end,
+			desc = "Peek Fold",
+		},
+	},
 	opts = {
 		provider_selector = function(bufnr, filetype, buftype)
 			return { "lsp", "indent" }
