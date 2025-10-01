4d02a4b22b9dda1d257994bd7c71a16e6d6d28eb
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-10-01 13:46:13 +0530

    few config things added to treesitter setup

--- config/nvim/lua/mgr8/plugins/treesitter.lua
+++ config/nvim/lua/mgr8/plugins/treesitter.lua
@@ -1,59 +1,63 @@
-return  {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
-  build = ":TSUpdate",
-  config = function() 
-    local configs = require("nvim-treesitter.configs")
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        "lua",
-        "vim",
-        "elixir",
-        "heex",
-        "python",
-        "go",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "dockerfile",
-        "gitignore",
-        "rust",
-        "sql",
-        "csv",
-        "fish",
-        "json",
-        "json5",
-        "jsonc",
-        "nix",
-        "toml",
-      },
+		configs.setup({
+			sync_install = true,
+			auto_install = true,
+			ignore_install = {},
+			modules = {},
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"vim",
+				"elixir",
+				"eex",
+				"heex",
+				"python",
+				"go",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"dockerfile",
+				"gitignore",
+				"rust",
+				"sql",
+				"csv",
+				"fish",
+				"json",
+				"json5",
+				"jsonc",
+				"nix",
+				"toml",
+			},
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
 					node_incremental = "<C-space>",
 					scope_incremental = false,
 					node_decremental = "<bs>",
-        },
-      },
-    })
-  end
-
+				},
+			},
+		})
+	end,
 }
