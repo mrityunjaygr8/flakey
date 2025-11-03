ec260f08a3a5300be81ac0755247b2d2b26bdb33
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-11-03 10:02:40 +0530

    opencode overlay commented out

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,24 +10,24 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    opencode = prev.opencode.overrideAttrs (oldAttrs: rec {
-      version = "0.14.3";
-      src = prev.fetchFromGitHub {
-        inherit (oldAttrs.src) owner repo;
-        rev = "v${version}";
-        sha256 = "sha256-cJKUaLIWexpG9Gz6ASIZ4SyoUe+1Ljf5pj/veyBi7Lg=";
-      };
-
-      tui = prev.buildGoModule {
-        inherit (oldAttrs.tui) name src modRoot subPackages ldflags installPhase;
-        vendorHash = "sha256-H+TybeyyHTbhvTye0PCDcsWkcN8M34EJ2ddxyXEJkZI=";
-      };
-      node_modules = prev.stdenvNoCC.mkDerivation {
-        inherit (oldAttrs.node_modules) name impureEnvVars nativeBuildInputs dontConfigure buildPhase installPhase dontFixup outputHashMode outputHashAlgo;
-
-        outputHash = "sha256-p01odCHK8++numXipx1p9qJ+bvZuGjBnV9GZRg0iQLY=";
-      };
-    });
+    # opencode = prev.opencode.overrideAttrs (oldAttrs: rec {
+    #   version = "0.14.3";
+    #   src = prev.fetchFromGitHub {
+    #     inherit (oldAttrs.src) owner repo;
+    #     rev = "v${version}";
+    #     sha256 = "sha256-cJKUaLIWexpG9Gz6ASIZ4SyoUe+1Ljf5pj/veyBi7Lg=";
+    #   };
+    #
+    #   tui = prev.buildGoModule {
+    #     inherit (oldAttrs.tui) name src modRoot subPackages ldflags installPhase;
+    #     vendorHash = "sha256-H+TybeyyHTbhvTye0PCDcsWkcN8M34EJ2ddxyXEJkZI=";
+    #   };
+    #   node_modules = prev.stdenvNoCC.mkDerivation {
+    #     inherit (oldAttrs.node_modules) name impureEnvVars nativeBuildInputs dontConfigure buildPhase installPhase dontFixup outputHashMode outputHashAlgo;
+    #
+    #     outputHash = "sha256-p01odCHK8++numXipx1p9qJ+bvZuGjBnV9GZRg0iQLY=";
+    #   };
+    # });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
