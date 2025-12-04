2fcb5e4767f1a48082b56c9b0d28056d93e66b1e
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-12-04 15:20:28 +0530

    iloader packaged using deb file now

--- pkgs/iloader.nix
+++ pkgs/iloader.nix
@@ -1,13 +1,59 @@
 {
-  appimageTools,
+  stdenv,
   fetchurl,
+  dpkg,
+  autoPatchelfHook,
+  wrapGAppsHook3,
+  webkitgtk_4_1,
+  gtk3,
+  openssl,
+  libappindicator-gtk3,
+  librsvg,
+  libsoup_3,
+  glib-networking,
+  xdotool,
 }: let
   pname = "iloader";
   version = "1.1.4";
 
   src = fetchurl {
-    url = "https://github.com/nab138/iloader/releases/download/v${version}/iloader-linux-amd64.AppImage";
-    hash = "";
+    url = "https://github.com/nab138/iloader/releases/download/v${version}/iloader-linux-amd64.deb";
+    sha256 = "sha256:898dcb5f8835536e0d0e88f024c8bad12cc71e04cc51f29541e9a1a17524e4f1";
   };
 in
-  appimageTools.wrapType2 {inherit pname version src;}
+  stdenv.mkDerivation {
+    inherit pname version src;
+
+    nativeBuildInputs = [
+      dpkg
+      autoPatchelfHook
+      wrapGAppsHook3
+    ];
+
+    buildInputs = [
+      gtk3
+      webkitgtk_4_1
+      openssl
+      libappindicator-gtk3
+      librsvg
+      libsoup_3
+      glib-networking
+      xdotool
+    ];
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      dpkg -x $src $out
+      cp -r $out/usr/share $out/share
+      cp -r $out/usr/bin $out/bin
+      rm -rf $out/usr
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "User friendly sideloader";
+      homepage = "https://github.com/nab138/iloader";
+      platforms = ["x86_64-linux"];
+    };
+  }
