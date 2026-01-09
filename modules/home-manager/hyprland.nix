d55f01fddbc7853e6ceccfb2970cf89e2cd6dc72
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-01-09 10:28:04 +0530

    hyprland config updated for new syntax

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -246,10 +246,10 @@ in {
       monitor = [",preffered,auto,1" "DP-2,preffered,auto,1,transform,3"];
       windowrule = [
         # Ignore maximize requests from apps. You'll probably like this.
-        "suppressevent maximize, class:.*"
+        "suppress_event maximize, match:class .*"
 
         # Fix some dragging issues with XWayland
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+        "no_focus on,match:class ^$,match:title ^$,match:xwayland 1,match:float 1,match:fullscreen 0,match:pin 0"
       ];
       bindm =
         # Move/resize windows with mainMod + LMB/RMB and dragging
