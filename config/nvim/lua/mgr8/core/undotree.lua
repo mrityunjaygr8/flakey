0bb4b9d5c52c45b56d8885cf3c286c3f8d2f8bb7
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-04-05 23:00:59 +0530

    undotree added

---
+++ config/nvim/lua/mgr8/core/undotree.lua
@@ -0,0 +1,16 @@
+-- require("undotree").enable()
+-- require("difftool").enable()
+vim.cmd("packadd nvim.undotree")
+
+local u = require("undotree")
+local wk = require("which-key")
+wk.add({
+	{
+		"<leader>u",
+		function()
+			u.open()
+		end,
+		desc = "Open UndoTree",
+		group = "[U]ndoTree",
+	},
+})
