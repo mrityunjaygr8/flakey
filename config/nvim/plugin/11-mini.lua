6fb9d8dab742d991932befe482bf123313eb5f8d
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-04-05 18:05:23 +0530

    using one pack.add for mini

--- config/nvim/plugin/11-mini.lua
+++ config/nvim/plugin/11-mini.lua
@@ -1,7 +1,9 @@
-vim.pack.add({ "https://github.com/nvim-mini/mini.statusline" })
-vim.pack.add({ "https://github.com/nvim-mini/mini.icons" })
-vim.pack.add({ "https://github.com/nvim-mini/mini.ai" })
-vim.pack.add({ "https://github.com/nvim-mini/mini.surround" })
+vim.pack.add({
+	"https://github.com/nvim-mini/mini.statusline",
+	"https://github.com/nvim-mini/mini.icons",
+	"https://github.com/nvim-mini/mini.ai",
+	"https://github.com/nvim-mini/mini.surround",
+})
 
 require("mini.statusline").setup({})
 require("mini.icons").setup({})
