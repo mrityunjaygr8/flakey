c4ad70ea1e5b91a65b16033d4b426d39657a357f
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-04-05 16:05:41 +0530

    correct config of snack smart picker

--- config/nvim/plugin/10-snacks.lua
+++ config/nvim/plugin/10-snacks.lua
@@ -20,8 +20,8 @@ local wk = require("which-key")
 wk.add({
   {"<leader>ee", function() s.explorer() end, desc = "Toggle Explorer"},
   {"<leader>lg", function() s.lazygit() end, desc = "Open [L]azy[G]it", group = "[L]azy[G]it"},
-  {"<leader>sb", function() s.pickers.buffers() end, desc = "Buffers", group = "[S]earch"},
-  {"<leader>sf", function() s.pickers.files() end, desc = "Find Files", group = "[S]earch"},
-  {"<leader>ss", function() s.pickers.grep() end, desc = "Grep", group = "[S]earch"},
-  {"<leader><leader>", function() s.pickers.smart() end, desc = "Smart Find Files", group = "[S]earch"},
+  {"<leader>sb", function() s.picker.buffers() end, desc = "Buffers", group = "[S]earch"},
+  {"<leader>sf", function() s.picker.files() end, desc = "Find Files", group = "[S]earch"},
+  {"<leader>ss", function() s.picker.grep() end, desc = "Grep", group = "[S]earch"},
+  {"<leader><space>", function() s.picker.smart() end, desc = "Smart Find Files", group = "[S]earch"},
 })
