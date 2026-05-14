36bd2b0cba11d1732e1c9907281edfe9d735f314
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-05-14 12:08:35 +0530

    csvview keybind added

--- config/nvim/plugin/81-csvview.lua
+++ config/nvim/plugin/81-csvview.lua
@@ -1,3 +1,15 @@
 vim.pack.add({ "https://github.com/hat0uma/csvview.nvim" })
+local wk = require("which-key")
 local csvview = require("csvview")
 csvview.setup({})
+
+wk.add({
+	{
+		"<leader>v",
+		function()
+			csvview.toggle()
+		end,
+		desc = "Toggle Csv[V]iew",
+		group = "Csv[V]iew",
+	},
+})
