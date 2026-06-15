ca39063baaed110ce1dde86f695a8ec37997dc22
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-06-15 15:07:14 +0530

    omp config added to config folder and being managed declaratively

--- modules/home-manager/omp.nix
+++ modules/home-manager/omp.nix
@@ -1,14 +1,22 @@
 {
   inputs,
   pkgs,
+  config,
   ...
-}: {
+}: let
+  cfg = config.home.homeDirectory;
+in {
   home.packages = [
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.omp
   ];
+
   sops.secrets."harness-api-keys" = {
     format = "dotenv";
     sopsFile = ../../secrets/home/mgr8/harness-api-keys.env;
-    path = "%r/harness-api-keys.env";
+    path = "${cfg}/.omp/agent/.env";
   };
+
+  home.file.".omp/agent/config.yml".source = ../../config/omp/config.yml;
+
+  home.file.".omp/agent/ssh.json".source = ../../config/omp/ssh.json;
 }
