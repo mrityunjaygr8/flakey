99f5b07f077a5690216e6aa38a661f524b5cba5d
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-06-27 15:09:29 +0530

    zmk studio added; mgr8 user added to uucp group

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -104,7 +104,7 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel" "docker"];
+      extraGroups = ["wheel" "docker" "uucp"];
     };
   };
 
@@ -126,6 +126,8 @@
     cachix
     procps
     usbutils
+    zmk-studio
+    zmkbatx
   ];
 
   hardware.logitech.wireless = {
