6983ca30b443a8396bf0169271d66945164ece58
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-08-04 13:57:50 +0530

    using opencode2 preview now

--- modules/home-manager/opencode.nix
+++ modules/home-manager/opencode.nix
@@ -8,17 +8,8 @@
     # package = inputs.opencode.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
     #   inherit (pkgs) bun;
     # };
-    package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode;
+    package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode2;
     enableMcpIntegration = true;
-    skills = {
-      graphify = ../../config/opencode/skills/graphify;
-    };
-    agents = {
-      graphify = ../../config/opencode/agents/graphify.md;
-    };
-    settings = {
-      plugin = ["./opencode/plugins/graphify"];
-    };
   };
   programs.mcp = {
     enable = true;
@@ -33,20 +24,13 @@
         command = "npx";
         args = ["-y" "chrome-devtools-mcp@latest" "--executablePath=${pkgs.chromium}/bin/chromium"];
       };
-      github = {
-        type = "remote";
-        url = "https://api.githubcopilot.com/mcp/";
-        headers = {
-          Authorization = "Bearer {env:GITHUB_TOKEN}";
-        };
-      };
-    };
-  };
-
-  xdg.configFile = {
-    "opencode/plugins" = {
-      source = ../../config/opencode/plugins;
-      recursive = true;
+      # github = {
+      #   type = "remote";
+      #   url = "https://api.githubcopilot.com/mcp/";
+      #   headers = {
+      #     Authorization = "Bearer {env:GITHUB_TOKEN}";
+      #   };
+      # };
     };
   };
 }
