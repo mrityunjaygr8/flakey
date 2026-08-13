5b48a31d777fc879910d847af187f9c5de58b2c6
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-08-13 13:34:54 +0530

    wf-recorder works with ffmpeg9

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,6 +7,17 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
+    # wf-recorder doesn't compile against FFmpeg 9 (removed AVCodec fields).
+    # Patch from Arch Linux to use avcodec_get_supported_config().
+    wf-recorder = prev.wf-recorder.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        ../patches/wf-recorder-ffmpeg9.patch
+      ];
+    });
+    # calibre doesn't compile against FFmpeg 9 (removed AVCodec fields)
+    calibre = prev.calibre.override {
+      ffmpeg = prev.ffmpeg_6;
+    };
     # bun = let
     #   version = "1.3.14";
     # in
